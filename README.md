797a346e58d5551707f63c97e0f073c5d1cf5187
Trang <quynhtranghbt@gmail.com>
2021-02-10 23:03:57 -0500

    updated README

--- README.md
+++ README.md
@@ -1,20 +1,45 @@
 [![Jovo Framework](https://www.jovo.tech/img/github-logo.png)](https://www.jovo.tech)
 
-<p align="center">Templates for the <a href="https://github.com/jovotech/jovo-framework-nodejs">Jovo Framework</a> ⭐️</p>
-
 <p align="center">
+Jovo Framework - 
 <a href="https://www.jovo.tech/framework/docs/"><strong>Documentation</strong></a> -
-<a href="https://github.com/jovotech/jovo-cli"><strong>CLI </strong></a> - <a href="https://github.com/jovotech/jovo-framework-nodejs/blob/master/CONTRIBUTING.md"><strong>Contributing</strong></a> - <a href="https://twitter.com/jovotech"><strong>Twitter</strong></a></p>
+<a href="https://github.com/jovotech/jovo-cli"><strong>CLI </strong></a>  </p>
 <br/>
 
-# Template: Hello World
+Hand Washing Helper skill for Google Home and Amazon Alexa build on Jovo Framework
+Voice app that helps people wash their hands the right way
+# HandWashingHelper
 
-Jovo Sample Voice App with a simple "Hello World!" + asking for the user's name. This is the default template for the `jovo new` command:
+## Project setup
 
-```sh
-$ jovo new <directory>
+Install Jovo
 
-## Alternative
-$ jovo new <directory> --template helloworld
+```
+npm install -g jovo-cli
 ```
 
+Navigate to HandWashingHelper folder
+
+```
+cd HandWashingHelper
+```
+
+Start server
+
+```
+jovo run
+```
+
+#
+
+## Voice Bot Platform
+
+### Alexa Developer Console
+
+https://developer.amazon.com/alexa/console/ask
+
+### Dialogflow (Google)
+
+https://dialogflow.cloud.google.com/
+
+#
--- src/app.js
+++ src/app.js
@@ -31,32 +31,17 @@ app.setHandler({
 
     HandsWashingIntent() {
 
-        this.$speech.addText(['Hand-washing can protect you from coronavirus. Let\'s do this right!', 'Hand-washing can protect you from coronavirus. Let\'s wash your hands the right way.'])
-        .addText('Are you ready?')
+        this.$speech.addText(['Hand-washing can protect you from coronavirus. Let\'s do this right!', 'Proper hand-washing can protect yourself from coronavirus. Let\'s wash your hands the right way.'])
 
         let title = 'Hand Washing Helper'
-        let content = 'Help people wash their hands the right way'
+        let content = 'Help people wash their hands properly'
         let imageUrl = 'https://trang102.s3.amazonaws.com/hand-washing-4818792_1920.jpg'
         this.showImageCard(title, content, imageUrl)
 
-        this.ask(this.$speech)
-       
-    },
-    NoIntent(){
-        this.$speech.addText("No problem.")
-        .addText("Let me know when you're ready or say STOP to close the app.")
 
-        this.ask(this.$speech)
-    },
 
-    StopIntent(){
-        this.$speech.addText("Goodbye! Stay safe and wear the mask!")
-        this.tell(this.$speech)
-    },
-
-    YesIntent(){
-        let goodbyeMessage = ["Stay safe and wear your mask. Goodbye!", "Congratulations, you got clean hands! And remember not to touch your face.", "Stay homme, stay safe!", "Stay safe and keep social distancing.", "Stay home, save lives!"]
-        this.$speech.addBreak("100ms")
+        let goodbyeMessage = ["Stay safe and wear your mask. Goodbye!", "Congratulations, you got clean hands! And remember not to touch your face.", "Stay home, stay safe!", "Stay safe and keep social distancing.", "Stay home, save lives!"]
+        this.$speech.addBreak("1000ms")
         .addText('Wet your hands with water and lather with some good soap.')
         .addBreak("100ms")
         .addAudio('https://trang102.s3.amazonaws.com/part1.mp3')
@@ -78,18 +63,30 @@ app.setHandler({
         .addText('Rinse your hands under running water.')
         .addBreak("100ms")
         .addAudio('https://trang102.s3.amazonaws.com/part7.mp3')
-        .addText('You are done. Now grab a clean towel or paper towel to dry your hands. Thank you for using Hand Washing Helper.')
+        .addBreak("1000ms")
+        .addText('Now grab a clean towel or paper towel to dry your hands. Thank you for using Hand Washing Helper.')
+        .addBreak('1000ms')
         .addText(goodbyeMessage)
 
-        let title = 'Hand Washing Helper'
-        let content = 'Wet and soap your hands.\n Rub two palms. \n Rub the back of your hands.\n Rub your fingers \n Rub your thumbs. \n Clean your nails. \n Rinse your hands. \n'
-        let imageUrl = 'https://trang102.s3.amazonaws.com/wash-hands-4919935_1920.jpg'
-        this.showImageCard(title, content, imageUrl)
+        // let title = 'Hand Washing Helper'
+        // let content = 'Wet and soap your hands.\n Rub two palms. \n Rub the back of your hands.\n Rub your fingers \n Rub your thumbs. \n Clean your nails. \n Rinse your hands. \n'
+        // let imageUrl = 'https://trang102.s3.amazonaws.com/wash-hands-4919935_1920.jpg'
+        // this.showImageCard(title, content, imageUrl)
 
         this.tell(this.$speech)
 
         // add image card    
-    }
+       
+    },
+
+    StopIntent(){
+        this.$speech.addText("Goodbye! Stay safe and wear the mask!")
+        this.tell(this.$speech)
+    },
+
+
+        
+    
 
     
 });
